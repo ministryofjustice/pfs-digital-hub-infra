468e19ee2372bb75052dc34fdcabf7ea5c7e24ae
Richard Dakin <Richard.Dakin@digital.justice.gov.uk>
2019-12-05 09:15:15 +0000

    missed dev

--- terraform/vault/main.tf
+++ terraform/vault/main.tf
@@ -29,6 +29,9 @@ data "azurerm_azuread_service_principal" "pfs-stage-1" {
 }
 
 
+data "azurerm_azuread_service_principal" "pfs-dev-1" {
+  display_name = "pfs-dev-digital-hub-1"
+}
 
 data "azuread_group" "DSDT" {
   name = "Digital Studio Dev Team - Digital Hub"
@@ -125,7 +128,14 @@ resource "azurerm_key_vault" "vault" {
       "get",
       "list"]
   }
+      access_policy {
+    object_id = "${data.azurerm_azuread_service_principal.pfs-dev-1.object_id}"
+    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
 
+    secret_permissions = [
+      "get",
+      "list"]
+  }
 
 
         enabled_for_template_deployment = false
