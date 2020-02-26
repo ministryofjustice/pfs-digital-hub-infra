e2fa9570a84c2ca7de66a8f652cc280373d89131
Richard Dakin <Richard.Dakin@digital.justice.gov.uk>
2020-02-26 09:02:05 +0000

    Updated Pauls permissions, removed Brett and removed some dead tasks

--- ansible/roles/azure/files/vault_password.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#This is used to get the password from the vault, if this fails you need to run az login from terminal before re-running ansible
-#unset HTTPS_PROXY
-az keyvault secret show --name pfshub-storage-prod-account-pw --vault-name pfs-hub-prod-hub-kv -o json 2>/dev/null | jq -rM '.value' | tr -d '\n'
