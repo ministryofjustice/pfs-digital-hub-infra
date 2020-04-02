a5dcc40aa7a93af0d233927d2ec1856abdb5ab8e
Richard Dakin <Richard.Dakin@digital.justice.gov.uk>
2020-04-02 14:19:20 +0100

    added email address

--- ansible/roles/docker/files/nginx/generate-dummy-certs.sh
+++ ansible/roles/docker/files/nginx/generate-dummy-certs.sh
@@ -7,7 +7,7 @@ fi
 domains=(pfs-digital-hub-stage.hmpps.dsd.io content.pfs-digital-hub-stage.hmpps.dsd.io analytics.pfs-digital-hub-stage.hmpps.dsd.io)
 rsa_key_size=4096
 data_path="/data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="thehub@digital.justice.gov.uk" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 if [ -d "$data_path" ]; then
     read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
